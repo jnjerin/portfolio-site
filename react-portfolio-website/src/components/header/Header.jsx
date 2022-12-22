abf208b6e94262c2aca16d8d7210cfd164bb18e7
jnjerin <njerijoan24@gmail.com>
2022-12-22 23:58:17 +0300

    Created the header component

--- react-portfolio-website/src/components/header/Header.jsx
+++ react-portfolio-website/src/components/header/Header.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
 
-const header = () => {
+const Header = () => {
   return (
-    <div>header</div>
+    <header>
+      <div className="container header__container">
+        <h5> Hello I'm</h5>
+        <h1>Joan Ndegwa</h1>
+        <h5 className="text-light">Fullstack Developer</h5>
+        <CTA />
+      </div> 
+    </header>
   )
 }
 
-export default header
+export default Header
