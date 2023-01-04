6c34f042f5ddc30b1a6a77a09161e04223988a23
jnjerin <njerijoan24@gmail.com>
2023-01-04 18:11:14 +0300

    nav styling done

--- react-portfolio-website/src/components/header/Header.jsx
+++ react-portfolio-website/src/components/header/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './header.css'
 import CTA from './CTA'
-import ME from '../../assets/me.jpg'
+import ME from '../../assets/me.png'
 import HeaderSocial from './HeaderSocials'
 
 const Header = () => {
@@ -17,7 +17,6 @@ const Header = () => {
         <div className="me">
           <img src={ME} alt="Joan" />
         </div>
-
          <a href='#contact' className='scroll__down'>Scroll Down</a> 
       </div> 
     </header> 
