e9c33a7aa5d91ac1ab13f0dccbd0f08b0b3cb6c7
jnjerin <njerijoan24@gmail.com>
2023-01-08 08:20:20 +0300

    created services content

--- react-portfolio-website/src/components/services/Services.jsx
+++ react-portfolio-website/src/components/services/Services.jsx
@@ -1,9 +1,135 @@
 import React from 'react'
 import './services.css'
+import {BiCheck} from 'react-icons/bi'
 
 const Services = () => {
   return (
-    <section id='services' >Services</section>
+    <section id='services' >
+      <h5>What I Offer</h5>
+      <h2>Services</h2>
+
+      <div className="container services__container">
+        <article className='service'>
+          <div className='service__head'>
+            <h3>Backend Development</h3>
+          </div>
+
+          <ul className='service_list'>
+            <li>
+              <BiCheck className='service__list-icon' />
+              <p>Lorem ipsum dolor sit amet consectetur.</p>
+            </li>
+
+            <li>
+              <BiCheck className='service__list-icon' />
+              <p>Lorem ipsum dolor sit amet consectetur.</p>
+            </li>
+
+            <li>
+              <BiCheck className='service__list-icon' />
+              <p>Lorem ipsum dolor sit amet consectetur.</p>
+            </li>
+
+            <li>
+              <BiCheck className='service__list-icon' />
+              <p>Lorem ipsum dolor sit amet consectetur.</p>
+            </li>
+
+            <li>
+              <BiCheck className='service__list-icon' />
+              <p>Lorem ipsum dolor sit amet consectetur.</p>
+            </li>
+
+            <li>
+              <BiCheck className='service__list-icon' />
+              <p>Lorem ipsum dolor sit amet consectetur.</p>
+            </li>
+          </ul>
+        </article>
+        
+        {/* END OF BACKEND DEVELOPMENT */}
+
+        <article className='service'>
+          <div className='service__head'>
+            <h3>Frontend Development</h3>
+          </div>
+
+          <ul className='service_list'>
+            <li>
+              <BiCheck className='service__list-icon' />
+              <p>Lorem ipsum dolor sit amet consectetur.</p>
+            </li>
+
+            <li>
+              <BiCheck className='service__list-icon' />
+              <p>Lorem ipsum dolor sit amet consectetur.</p>
+            </li>
+
+            <li>
+              <BiCheck className='service__list-icon' />
+              <p>Lorem ipsum dolor sit amet consectetur.</p>
+            </li>
+
+            <li>
+              <BiCheck className='service__list-icon' />
+              <p>Lorem ipsum dolor sit amet consectetur.</p>
+            </li>
+
+            <li>
+              <BiCheck className='service__list-icon' />
+              <p>Lorem ipsum dolor sit amet consectetur.</p>
+            </li>
+
+            <li>
+              <BiCheck className='service__list-icon' />
+              <p>Lorem ipsum dolor sit amet consectetur.</p>
+            </li>
+          </ul>
+        </article>
+
+        {/* END OF FRONTEND DEVELOPMENT */}   
+
+        <article className='service'>
+          <div className='service__head'>
+            <h3>DevOps</h3>
+          </div>
+
+          <ul className='service_list'>
+            <li>
+              <BiCheck className='service__list-icon' />
+              <p>Lorem ipsum dolor sit amet consectetur.</p>
+            </li>
+
+            <li>
+              <BiCheck className='service__list-icon' />
+              <p>Lorem ipsum dolor sit amet consectetur.</p>
+            </li>
+
+            <li>
+              <BiCheck className='service__list-icon' />
+              <p>Lorem ipsum dolor sit amet consectetur.</p>
+            </li>
+
+            <li>
+              <BiCheck className='service__list-icon' />
+              <p>Lorem ipsum dolor sit amet consectetur.</p>
+            </li>
+
+            <li>
+              <BiCheck className='service__list-icon' />
+              <p>Lorem ipsum dolor sit amet consectetur.</p>
+            </li>
+
+            <li>
+              <BiCheck className='service__list-icon' />
+              <p>Lorem ipsum dolor sit amet consectetur.</p>
+            </li>
+          </ul>
+        </article>
+
+        {/* END OF DEVOPS */}   
+      </div>
+    </section>
   )
 }
 
