a568cf6b5aecf8ac16c619710a1709c66478c8dc
jnjerin <njerijoan24@gmail.com>
2023-01-16 22:59:53 +0300

    Portfolio styling done

--- react-portfolio-website/src/components/portfolio/Portfolio.jsx
+++ react-portfolio-website/src/components/portfolio/Portfolio.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import './portfolio.css'
-import IMG1 from '../../assets/portfolio1.jpg'
-import IMG2 from '../../assets/portfolio2.jpg'
-import IMG3 from '../../assets/portfolio3.jpg'
-import IMG4 from '../../assets/portfolio4.jpg'
-import IMG5 from '../../assets/portfolio5.jpg'
-import IMG6 from '../../assets/portfolio6.jpg'
+import IMG1 from '../../assets/portfolio1.PNG'
+import IMG2 from '../../assets/portfolio2.PNG'
+import IMG3 from '../../assets/portfolio3.PNG'
+import IMG4 from '../../assets/portfolio4.PNG'
+import IMG5 from '../../assets/portfolio5.PNG'
+import IMG6 from '../../assets/portfolio6.PNG'
 
 
 
@@ -18,12 +18,69 @@ const Portfolio = () => {
       <div className="container portfolio__container">
         <article className='portfolio__item'>
           <div className="portfolio__item-image">
-            <h3>This is a portfolio item title</h3>
+            <img src={IMG1} alt="" />
+          </div> 
+          <h3>This is a portfolio item title</h3>
+          <div className="portfolio__item-cta">
             <a href="https://github.com" className='btn' target='_blank'>Github</a>
             <a href="https://dribbble.com/Alien_pixels" className='btn btn-primary' target='_blank'>Live Demo</a>
-          </div>
-
+          </div>           
         </article>
+
+        <article className='portfolio__item'>
+          <div className="portfolio__item-image">
+            <img src={IMG2} alt="" />
+          </div> 
+          <h3>This is a portfolio item title</h3>
+          <div className="portfolio__item-cta">
+            <a href="https://github.com" className='btn' target='_blank'>Github</a>
+            <a href="https://dribbble.com/Alien_pixels" className='btn btn-primary' target='_blank'>Live Demo</a>
+          </div> 
+        </article>
+
+        <article className='portfolio__item'>
+          <div className="portfolio__item-image">
+            <img src={IMG3} alt="" />
+          </div> 
+          <h3>This is a portfolio item title</h3>
+          <div className="portfolio__item-cta">
+            <a href="https://github.com" className='btn' target='_blank'>Github</a>
+            <a href="https://dribbble.com/Alien_pixels" className='btn btn-primary' target='_blank'>Live Demo</a>
+          </div> 
+        </article>
+
+        <article className='portfolio__item'>
+          <div className="portfolio__item-image">
+            <img src={IMG4} alt="" />
+          </div> 
+          <h3>This is a portfolio item title</h3>
+          <div className="portfolio__item-cta">
+            <a href="https://github.com" className='btn' target='_blank'>Github</a>
+            <a href="https://dribbble.com/Alien_pixels" className='btn btn-primary' target='_blank'>Live Demo</a>
+          </div> 
+        </article>
+
+        <article className='portfolio__item'>
+          <div className="portfolio__item-image">
+            <img src={IMG5} alt="" />
+          </div> 
+          <h3>This is a portfolio item title</h3>
+          <div className="portfolio__item-cta">
+            <a href="https://github.com" className='btn' target='_blank'>Github</a>
+            <a href="https://dribbble.com/Alien_pixels" className='btn btn-primary' target='_blank'>Live Demo</a>
+          </div> 
+        </article>
+
+        <article className='portfolio__item'>
+          <div className="portfolio__item-image">
+            <img src={IMG6} alt="" />
+          </div> 
+          <h3>This is a portfolio item title</h3>
+          <div className="portfolio__item-cta">
+            <a href="https://github.com" className='btn' target='_blank'>Github</a>
+            <a href="https://dribbble.com/Alien_pixels" className='btn btn-primary' target='_blank'>Live Demo</a>
+          </div> 
+        </article>        
       </div>
     </section>
   )
