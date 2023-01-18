c0820c89cd8ee59d0aa2f2e72b1bb8fffa86e3be
jnjerin <njerijoan24@gmail.com>
2023-01-18 23:28:36 +0300

    Added contact form markup

--- react-portfolio-website/src/components/contact/Contact.jsx
+++ react-portfolio-website/src/components/contact/Contact.jsx
@@ -14,13 +14,13 @@ const Contact = () => {
             <MdOutlineEmail />
             <h4>Email</h4>
             <h5>njerijoan24@gmail.com</h5>
-            <a href="mailto:njerijoan24@gmail.com">Send a Message</a>
+            <a href="mailto:njerijoan24@gmail.com" target="_blank">Send a Message</a>
           </article>
           <article className="contact__option">
             <MdOutlineEmail />
             <h4>Twitter</h4>
             <h5>DM on Twitter</h5>
-            <a href="https://twitter.com/messages/compose?recipient_id=&ref_src=twsrc%5Etfw" class="twitter-dm-button" data-screen-name="joanndegwa" data-show-count="false">Message @joanndegwa</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+            <a href="https://twitter.com/messages/compose?recipient_id=&ref_src=twsrc%5Etfw" class="twitter-dm-button" data-screen-name="joanndegwa" data-show-count="false" target="_blank">Message @joanndegwa</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
           </article>
           {/* <article className="contact__option">
             <MdOutlineEmail />
@@ -32,7 +32,12 @@ const Contact = () => {
         </div>
 
         {/* END OF CONTACT OPTIONS */}
-        <form action=''></form>
+        <form action=''> 
+          <input type="text" name='name' placeholder='Your Full Name' required/>
+          <input type="email" name='email' placeholder='Your Email' />
+          <textarea name='message' rows='7' placeholder='Your Message' required> </textarea>
+          <button type='submit' className='btn btn-primary' >Send Message</button>
+        </form>
       </div>
     </section>
   )
